0857db493f671422a833d947aef93d305749247f
ehspill <81327299+ehspill@users.noreply.github.com>
2025-03-25 21:36:50 +0100

    Delete EntryPoints/01_wifi_test.sh

--- EntryPoints/01_wifi_test.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-mount --bind /mnt/tempshadow /etc/jffs2/shadow
-mount --bind /mnt/tempwificreds /etc/jffs2/anyka_cfg.ini
-/bin/tcpsvd 0 21 ftpd -w / -t 600 &
-/sbin/telnetd &
-kill cmd_serverd
-/usr/sbin/net_manage.sh &
